5cd3725a119104ccbce4b0381821ccdb598942fb
Ridho Aulia Rahman <220605110044@student.uin-malang.ac.id>
2024-01-05 18:32:17 +0700

    summary

--- adminpanel/index.php
+++ adminpanel/index.php
@@ -9,23 +9,53 @@ require "session.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
-    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
 </head>
+<style>
+    .kotak {
+        border: solid;
+    }
+
+    .summary-kategori {
+        background-color: #0a6b4a;
+    }
+</style>
 
 <body>
+    <!-- Navbar-Start -->
     <?php require "navbar.php"; ?>
+    <!-- Navbar End -->
+
     <div class="container mt-5">
         <nav aria-label="breadcrumb">
             <ol class="breadcrumb">
-                <li class="breadcrumb-item active" aria-current="page">Home</li>
+                <li class="breadcrumb-item active" aria-current="page">
+                    <i class="fa-solid fa-house"></i> Home
+                </li>
             </ol>
         </nav>
-
         <h2>Halo <?php echo $_SESSION['username']; ?></h2>
+
+        <div class="container mt-5 summary-kategori p-3">
+            <div class="row">
+                <div class="col-lg-4 kotak">
+                    <div class="row">
+                        <div class="col-6">
+                            <i class="fa-solid fa-align-justify fa-5x"></i>
+                        </div>
+                        <div class="col-lg-6">
+                           
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
 
-    <script src="../bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/3e2c450e53.js" crossorigin="anonymous"></script>
 </body>
 
 </html>
